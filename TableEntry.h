dc179b34c6e1751cbcc396a203b7b1f480ba9d37
Unai Molla Caballero <umolcab@edu.upv.es>
2024-01-10 18:33:53 +0100

    Ficheros creados pero me da errores que no sonsigo detectar

--- TableEntry.h
+++ TableEntry.h
@@ -13,7 +13,9 @@ class TableEntry {
 
 		TableEntry(std::string key) : key{key} {}
 
-		TableEntry();
+		TableEntry(){
+			key = "";
+		}
 
 		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
 			return te1.key == te2.key;
@@ -25,6 +27,14 @@ class TableEntry {
 		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
 			out << "\nKey: " << te.key << " ,Value: " << te.value;
 			return out;
+		}
+
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key < te2.key;
+		}
+		
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key > te2.key;
 		}
 };
 
